893473e26d9e706184902a64fc7987f5efee8a92
leedo97y <dlehdus1433@gmail.com>
2022-04-20 15:55:33 +0900

    add api

--- practice/script.js
+++ practice/script.js
@@ -4,4 +4,22 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const getPost = async () => {
+    const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+    const response = await fetch(API_URL)
+    if (!response.ok) {
+      throw new Error('에러')
+    }
+    return await response.json()
+  }
+
+  const loadPost = async () => {
+    const response = await getPost()
+    console.log(response)
+  }
+
+  window.addEventListener('DOMcontentLoaded', () => {
+    loadPost()
+  })
 })()
